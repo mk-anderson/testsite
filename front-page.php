6d6673e755965822263f65bae34ef9ccb528ab14
mk-anderson <mk.anderson@hama-x.co.jp>
2025-09-24 17:07:09 +0900

    バグ修正

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php
+// front-page.php : トップページ専用テンプレート
+
+get_header('top'); // ← header-top.php を呼び出す
+?>
+
+<main class="p-home">
+  <h1>トップページです</h1>
+  <p>ここにトップ専用のコンテンツを置きます。</p>
+</main>
+
+<?php get_footer(); ?>
