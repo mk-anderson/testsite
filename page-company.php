486f88e602f05750d5eb14d00e953a3538676806
mk-anderson <mk.anderson@hama-x.co.jp>
2025-10-15 16:26:32 +0900

    固定ページタイトル背景画像追加(実装未完成)

--- page-company.php
+++ page-company.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
 <main class="p-main">
-  <h1 class="u-text-color1 u-text-fntfam1"><?php the_title(); ?></h1>
+  <h1 class="u-text-color1 u-text-fntfam1">
+    <?php the_title(); ?>
+    <picture>
+				<source class="p-pagetitle" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/img-pagehead_back.jpg" media="(min-width: 376px)" alt="ページタイトル背景">
+		</picture>
+  </h1>
   <p><?php bloginfo('name'); ?></p>
   <p><?php bloginfo('description'); ?></p>
   <?php if (have_posts()) : while (have_posts()) : the_post(); the_content(); endwhile; endif; ?>
